42e881c241a82a7631b6eaf6f89b5d08d98c0ddb
Sir-Jarvis <sirjarvis@anything.com>
2021-12-23 15:20:38 +0000

    last deployment commit

--- accounts/api.py
+++ accounts/api.py
@@ -1,7 +1,6 @@
 from rest_framework import generics,permissions
 from rest_framework.response import Response
 
-
 from .serializers import *
 from .models import *
 from rest_framework.views import APIView
